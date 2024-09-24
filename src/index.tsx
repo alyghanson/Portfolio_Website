5b6569202b12892fc41243a4488b9841fc2564cc
Alyssa Hanson <alyghanson@gmail.com>
2024-09-23 22:06:14 -0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/Index.css';
 import App from './App';
-import { HashRouter as Router } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
