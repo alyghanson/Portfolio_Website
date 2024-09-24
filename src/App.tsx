62d641ec561ab275a4efe34a32c1b4045d4ec1b0
Alyssa Hanson <alyghanson@gmail.com>
2024-09-23 22:19:37 -0700

    routing issue address

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 
 import Resume from "./pages/Resume";
 import Contact from './pages/Contact';
@@ -9,16 +9,14 @@ import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route index element={<Intro />} />
-        <Route path="/" element={<Intro/>}/> 
-        <Route path="/home" element={<Home/>}/> 
-        <Route path="/resume" element={<Resume/>} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="*" element={<NoPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route index element={<Intro />} />
+      <Route path="/" element={<Intro/>}/> 
+      <Route path="/home" element={<Home/>}/> 
+      <Route path="/resume" element={<Resume/>} />
+      <Route path="/contact" element={<Contact/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="*" element={<NoPage />} />
+    </Routes>
   );
 }
