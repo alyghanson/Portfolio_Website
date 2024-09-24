cf867fc6350b2c6b90f9c7f7cf51e23055486576
Alyssa Hanson <alyghanson@gmail.com>
2024-09-23 22:31:28 -0700

    browserrouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/Index.css';
 import App from './App';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
     const root = ReactDOM.createRoot(rootElement);
     root.render(
-      <Router>
+      <BrowserRouter basename='/Portfolio_Website'>
         <App />
-      </Router>
+      </BrowserRouter>
     );
 }
